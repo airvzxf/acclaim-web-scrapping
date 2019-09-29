4ed182a95d5a85594fd00c00e4809706433cc28a
Israel Roldan <israel.alberto.rv@gmail.com>
2019-09-29 13:23:01 -0500

    Refactored the mock usage, now I am using the Mock class for the imported modules.

--- src/test/HttpRequestTestCase.py
+++ src/test/HttpRequestTestCase.py
@@ -26,7 +26,6 @@ class HttpRequestTestCase(TestCase):
 
         self.url_2 = 'https://fake-news.org'
         self.status_2 = 9999
-        self.data_2 = b'I am not new!'
         self.data_json_2 = b'{' \
                            b'"nothing":[{"no":true}, {"thing":false}]' \
                            b'}'
@@ -37,96 +36,96 @@ class HttpRequestTestCase(TestCase):
         http_request = HttpRequest(url=self.url_1)
         self.assertEqual(self.url_1, http_request.url)
 
-    @patch('urllib3.PoolManager.request')
-    def test_request_url_and_get_the_response(self, urllib3_patch):
-        mock_urllib3 = Mock()
-        mock_urllib3.status = self.status_1
-        mock_urllib3.data = None
-        urllib3_patch.return_value = mock_urllib3
+    @patch('urllib3.PoolManager')
+    def test_request_url_and_get_the_response(self, mock_urllib3):
+        mock_urllib3.return_value = Mock()
+        mock_urllib3_class_request = mock_urllib3.return_value.request.return_value
+        mock_urllib3_class_request.status = self.status_1
+        mock_urllib3_class_request.data = None
 
         http_request = HttpRequest(url=self.url_1)
         response = http_request.request()
         self.assertEqual(self.status_1, response)
 
-    @patch('urllib3.PoolManager.request')
-    def test_request_another_url_and_get_the_response(self, urllib3_patch):
-        mock_urllib3 = Mock()
-        mock_urllib3.status = self.status_2
-        mock_urllib3.data = None
-        urllib3_patch.return_value = mock_urllib3
+    @patch('urllib3.PoolManager')
+    def test_request_another_url_and_get_the_response(self, mock_urllib3):
+        mock_urllib3.return_value = Mock()
+        mock_urllib3_class_request = mock_urllib3.return_value.request.return_value
+        mock_urllib3_class_request.status = self.status_2
+        mock_urllib3_class_request.data = None
 
         http_request = HttpRequest(url=self.url_2)
         response = http_request.request()
         self.assertEqual(self.status_2, response)
 
-    @patch('urllib3.PoolManager.request')
-    def test_get_response_data(self, urllib3_patch):
-        mock_urllib3 = Mock()
-        mock_urllib3.data = self.data_1
-        urllib3_patch.return_value = mock_urllib3
+    @patch('urllib3.PoolManager')
+    def test_get_response_data(self, mock_urllib3):
+        mock_urllib3.return_value = Mock()
+        mock_urllib3_class_request = mock_urllib3.return_value.request.return_value
+        mock_urllib3_class_request.data = self.data_1
 
         http_request = HttpRequest(url=self.url_1)
         http_request.request()
         data = http_request.data
         self.assertEqual(self.data_1, data)
 
-    @patch('urllib3.PoolManager.request')
-    def test_get_response_data_with_json_object(self, urllib3_patch):
+    @patch('urllib3.PoolManager')
+    def test_get_response_data_with_json_object(self, mock_urllib3):
         from json import loads
         expected_data_json = loads('{"nothing":[{"no":true}, {"thing":false}]}')
 
-        mock_urllib3 = Mock()
-        mock_urllib3.data = self.data_json_2
-        urllib3_patch.return_value = mock_urllib3
+        mock_urllib3.return_value = Mock()
+        mock_urllib3_class_request = mock_urllib3.return_value.request.return_value
+        mock_urllib3_class_request.data = self.data_json_2
 
         http_request = HttpRequest(url=self.url_1)
         http_request.request()
         data_json = http_request.data_json
         self.assertEqual(expected_data_json, data_json)
 
-    @patch('urllib3.PoolManager.request')
-    def test_get_response_data_with_invalid_json_object(self, urllib3_patch):
+    @patch('urllib3.PoolManager')
+    def test_get_response_data_with_invalid_json_object(self, mock_urllib3):
         expected_data_json = self.data_json_empty
 
-        mock_urllib3 = Mock()
-        mock_urllib3.data = self.data_1
-        urllib3_patch.return_value = mock_urllib3
+        mock_urllib3.return_value = Mock()
+        mock_urllib3_class_request = mock_urllib3.return_value.request.return_value
+        mock_urllib3_class_request.data = self.data_1
 
         http_request = HttpRequest(url=self.url_1)
         http_request.request()
         data_json = http_request.data_json
         self.assertEqual(expected_data_json, data_json)
 
-    @patch('urllib3.PoolManager.request')
-    def test_get_response_invalid_data_check_the_json_object(self, urllib3_patch):
-        mock_urllib3 = Mock()
-        mock_urllib3.data = None
-        urllib3_patch.return_value = mock_urllib3
+    @patch('urllib3.PoolManager')
+    def test_get_response_invalid_data_check_the_json_object(self, mock_urllib3):
+        mock_urllib3.return_value = Mock()
+        mock_urllib3_class_request = mock_urllib3.return_value.request.return_value
+        mock_urllib3_class_request.data = None
 
         http_request = HttpRequest(url=self.url_1)
         http_request.request()
         data_json = http_request.data_json
         self.assertEqual(self.data_json_empty, data_json)
 
-    @patch('urllib3.PoolManager.request')
-    def test_get_response_json_for_data_object(self, urllib3_patch):
+    @patch('urllib3.PoolManager')
+    def test_get_response_json_for_data_object(self, mock_urllib3):
         from json import loads
         expected_data_json = loads('{"data":[{"hi":"Man"}, {"orko":null}]}')['data']
 
-        mock_urllib3 = Mock()
-        mock_urllib3.data = self.data_json_1
-        urllib3_patch.return_value = mock_urllib3
+        mock_urllib3.return_value = Mock()
+        mock_urllib3_class_request = mock_urllib3.return_value.request.return_value
+        mock_urllib3_class_request.data = self.data_json_1
 
         http_request = HttpRequest(url=self.url_1)
         http_request.request()
         data_json = http_request.get_data_json_by('data')
         self.assertEqual(expected_data_json, data_json)
 
-    @patch('urllib3.PoolManager.request')
-    def test_get_response_json_for_data_object_when_not_exists(self, urllib3_patch):
-        mock_urllib3 = Mock()
-        mock_urllib3.data = self.data_json_2
-        urllib3_patch.return_value = mock_urllib3
+    @patch('urllib3.PoolManager')
+    def test_get_response_json_for_data_object_when_not_exists(self, mock_urllib3):
+        mock_urllib3.return_value = Mock()
+        mock_urllib3_class_request = mock_urllib3.return_value.request.return_value
+        mock_urllib3_class_request.data = self.data_json_2
 
         http_request = HttpRequest(url=self.url_1)
         http_request.request()
--- src/test/acclaim/badges/GetInformationTestCase.py
+++ src/test/acclaim/badges/GetInformationTestCase.py
@@ -10,41 +10,41 @@ from app.acclaim.badges.GetInformation import GetInformation
 
 
 class GetInformationTestCase(TestCase):
-    @patch('urllib3.PoolManager.request')
-    def test_get_number_of_badges(self, urllib3_patch):
+    @patch('urllib3.PoolManager')
+    def test_get_number_of_badges(self, mock_urllib3):
         expected_badges = 1952
         response_data = bytes(
             'This is a cool test {badges} badges in our property.'.format(badges=expected_badges),
             'utf-8')
 
-        mock_urllib3 = Mock()
-        mock_urllib3.data = response_data
-        urllib3_patch.return_value = mock_urllib3
+        mock_urllib3.return_value = Mock()
+        mock_urllib3_class_request = mock_urllib3.return_value.request.return_value
+        mock_urllib3_class_request.data = response_data
 
         get_information = GetInformation('Any url')
         badges = get_information.badges
         self.assertEqual(expected_badges, badges)
 
-    @patch('urllib3.PoolManager.request')
-    def test_when_the_page_not_has_the_information(self, urllib3_patch):
+    @patch('urllib3.PoolManager')
+    def test_when_the_page_not_has_the_information(self, mock_urllib3):
         expected_badges = 0
         response_data = b'Anything here.'
 
-        mock_urllib3 = Mock()
-        mock_urllib3.data = response_data
-        urllib3_patch.return_value = mock_urllib3
+        mock_urllib3.return_value = Mock()
+        mock_urllib3_class_request = mock_urllib3.return_value.request.return_value
+        mock_urllib3_class_request.data = response_data
 
         get_information = GetInformation('Any url')
         badges = get_information.badges
         self.assertEqual(expected_badges, badges)
 
-    @patch('urllib3.PoolManager.request')
-    def test_when_the_data_response_is_wrong(self, urllib3_patch):
+    @patch('urllib3.PoolManager')
+    def test_when_the_data_response_is_wrong(self, mock_urllib3):
         expected_badges = 0
 
-        mock_urllib3 = Mock()
-        mock_urllib3.data = False
-        urllib3_patch.return_value = mock_urllib3
+        mock_urllib3.return_value = Mock()
+        mock_urllib3_class_request = mock_urllib3.return_value.request.return_value
+        mock_urllib3_class_request.data = False
 
         get_information = GetInformation('Any url')
         badges = get_information.badges
