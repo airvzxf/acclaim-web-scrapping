9c7105d18055dde523a39825a9cc101bd9572f3c
Israel Roldan <israel.alberto.rv@gmail.com>
2019-09-29 20:11:49 -0500

    Refactored test cases for Http request using setup and tear down for the patch modules.

--- src/test/HttpRequestTestCase.py
+++ src/test/HttpRequestTestCase.py
@@ -4,7 +4,7 @@
 Unit Test for the HttpRequest class.
 """
 from unittest import TestCase
-from unittest.mock import patch, Mock
+from unittest.mock import patch
 
 from app.HttpRequest import HttpRequest
 
@@ -16,6 +16,10 @@ class HttpRequestTestCase(TestCase):
         :rtype: None
         :return: None
         """
+        self.patch_urllib3 = patch('urllib3.PoolManager')
+        self.MockUrllib3 = self.patch_urllib3.start()
+        self.MockUrllib3_request = self.MockUrllib3.return_value.request.return_value
+
         self.url_1 = 'https://fake.me'
         self.status_1 = 10101
         self.data_1 = b'My body is not fake'
@@ -32,100 +36,84 @@ class HttpRequestTestCase(TestCase):
 
         self.data_json_empty = {}
 
+    def tearDown(self) -> None:
+        """
+        Reset the test when it finish.
+        :rtype: None
+        :return: None
+        """
+        self.patch_urllib3.stop()
+
     def test_assign_the_url_variable(self):
         http_request = HttpRequest(url=self.url_1)
         self.assertEqual(self.url_1, http_request.url)
 
-    @patch('urllib3.PoolManager')
-    def test_request_url_and_get_the_response(self, mock_urllib3):
-        mock_urllib3.return_value = Mock()
-        mock_urllib3_class_request = mock_urllib3.return_value.request.return_value
-        mock_urllib3_class_request.status = self.status_1
-        mock_urllib3_class_request.data = None
+    def test_request_url_and_get_the_response(self):
+        self.MockUrllib3_request.status = self.status_1
+        self.MockUrllib3_request.data = None
 
         http_request = HttpRequest(url=self.url_1)
         response = http_request.request()
         self.assertEqual(self.status_1, response)
 
-    @patch('urllib3.PoolManager')
-    def test_request_another_url_and_get_the_response(self, mock_urllib3):
-        mock_urllib3.return_value = Mock()
-        mock_urllib3_class_request = mock_urllib3.return_value.request.return_value
-        mock_urllib3_class_request.status = self.status_2
-        mock_urllib3_class_request.data = None
+    def test_request_another_url_and_get_the_response(self):
+        self.MockUrllib3_request.status = self.status_2
+        self.MockUrllib3_request.data = None
 
         http_request = HttpRequest(url=self.url_2)
         response = http_request.request()
         self.assertEqual(self.status_2, response)
 
-    @patch('urllib3.PoolManager')
-    def test_get_response_data(self, mock_urllib3):
-        mock_urllib3.return_value = Mock()
-        mock_urllib3_class_request = mock_urllib3.return_value.request.return_value
-        mock_urllib3_class_request.data = self.data_1
+    def test_get_response_data(self):
+        self.MockUrllib3_request.data = self.data_1
 
         http_request = HttpRequest(url=self.url_1)
         http_request.request()
         data = http_request.data
         self.assertEqual(self.data_1, data)
 
-    @patch('urllib3.PoolManager')
-    def test_get_response_data_with_json_object(self, mock_urllib3):
+    def test_get_response_data_with_json_object(self):
         from json import loads
         expected_data_json = loads('{"nothing":[{"no":true}, {"thing":false}]}')
 
-        mock_urllib3.return_value = Mock()
-        mock_urllib3_class_request = mock_urllib3.return_value.request.return_value
-        mock_urllib3_class_request.data = self.data_json_2
+        self.MockUrllib3_request.data = self.data_json_2
 
         http_request = HttpRequest(url=self.url_1)
         http_request.request()
         data_json = http_request.data_json
         self.assertEqual(expected_data_json, data_json)
 
-    @patch('urllib3.PoolManager')
-    def test_get_response_data_with_invalid_json_object(self, mock_urllib3):
+    def test_get_response_data_with_invalid_json_object(self):
         expected_data_json = self.data_json_empty
 
-        mock_urllib3.return_value = Mock()
-        mock_urllib3_class_request = mock_urllib3.return_value.request.return_value
-        mock_urllib3_class_request.data = self.data_1
+        self.MockUrllib3_request.data = self.data_1
 
         http_request = HttpRequest(url=self.url_1)
         http_request.request()
         data_json = http_request.data_json
         self.assertEqual(expected_data_json, data_json)
 
-    @patch('urllib3.PoolManager')
-    def test_get_response_invalid_data_check_the_json_object(self, mock_urllib3):
-        mock_urllib3.return_value = Mock()
-        mock_urllib3_class_request = mock_urllib3.return_value.request.return_value
-        mock_urllib3_class_request.data = None
+    def test_get_response_invalid_data_check_the_json_object(self):
+        self.MockUrllib3_request.data = None
 
         http_request = HttpRequest(url=self.url_1)
         http_request.request()
         data_json = http_request.data_json
         self.assertEqual(self.data_json_empty, data_json)
 
-    @patch('urllib3.PoolManager')
-    def test_get_response_json_for_data_object(self, mock_urllib3):
+    def test_get_response_json_for_data_object(self):
         from json import loads
         expected_data_json = loads('{"data":[{"hi":"Man"}, {"orko":null}]}')['data']
 
-        mock_urllib3.return_value = Mock()
-        mock_urllib3_class_request = mock_urllib3.return_value.request.return_value
-        mock_urllib3_class_request.data = self.data_json_1
+        self.MockUrllib3_request.data = self.data_json_1
 
         http_request = HttpRequest(url=self.url_1)
         http_request.request()
         data_json = http_request.get_data_json_by('data')
         self.assertEqual(expected_data_json, data_json)
 
-    @patch('urllib3.PoolManager')
-    def test_get_response_json_for_data_object_when_not_exists(self, mock_urllib3):
-        mock_urllib3.return_value = Mock()
-        mock_urllib3_class_request = mock_urllib3.return_value.request.return_value
-        mock_urllib3_class_request.data = self.data_json_2
+    def test_get_response_json_for_data_object_when_not_exists(self):
+        self.MockUrllib3_request.data = self.data_json_2
 
         http_request = HttpRequest(url=self.url_1)
         http_request.request()
