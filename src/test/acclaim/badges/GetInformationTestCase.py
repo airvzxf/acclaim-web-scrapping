f76208c9bcd47aca6c3cfd9f520b8c1574d2d921
Israel Roldan <israel.alberto.rv@gmail.com>
2019-09-30 22:01:19 -0500

    Refactored the test case Get information moving the mock patch into a setup.

--- src/test/acclaim/badges/GetInformationTestCase.py
+++ src/test/acclaim/badges/GetInformationTestCase.py
@@ -4,48 +4,56 @@
 Unit Test for the GetInformation class.
 """
 from unittest import TestCase
-from unittest.mock import patch, Mock
+from unittest.mock import patch
 
 from app.acclaim.badges.GetInformation import GetInformation
 
 
-# TODO: Move Mock Patch into a setup function.
 class GetInformationTestCase(TestCase):
-    @patch('urllib3.PoolManager', autospec=True)
-    def test_get_number_of_badges(self, mock_urllib3):
+    def setUp(self) -> None:
+        """
+        Set up the Test Case.
+        :rtype: None
+        :return: None
+        """
+        self.patch_urllib3 = patch('urllib3.PoolManager', autospec=True)
+        self.MockUrllib3 = self.patch_urllib3.start()
+        self.MockUrllib3_request = self.MockUrllib3.return_value.request.return_value
+
+    def tearDown(self) -> None:
+        """
+        Reset the test when it finish.
+        :rtype: None
+        :return: None
+        """
+        self.patch_urllib3.stop()
+
+    def test_get_number_of_badges(self):
         expected_badges = 1952
         response_data = bytes(
             'This is a cool test {badges} badges in our property.'.format(badges=expected_badges),
             'utf-8')
 
-        mock_urllib3.return_value = Mock()
-        mock_urllib3_class_request = mock_urllib3.return_value.request.return_value
-        mock_urllib3_class_request.data = response_data
+        self.MockUrllib3_request.data = response_data
 
         get_information = GetInformation('Any url')
         badges = get_information.badges
         self.assertEqual(expected_badges, badges)
 
-    @patch('urllib3.PoolManager', autospec=True)
-    def test_when_the_page_not_has_the_information(self, mock_urllib3):
+    def test_when_the_page_not_has_the_information(self):
         expected_badges = 0
         response_data = b'Anything here.'
 
-        mock_urllib3.return_value = Mock()
-        mock_urllib3_class_request = mock_urllib3.return_value.request.return_value
-        mock_urllib3_class_request.data = response_data
+        self.MockUrllib3_request.data = response_data
 
         get_information = GetInformation('Any url')
         badges = get_information.badges
         self.assertEqual(expected_badges, badges)
 
-    @patch('urllib3.PoolManager', autospec=True)
-    def test_when_the_data_response_is_wrong(self, mock_urllib3):
+    def test_when_the_data_response_is_wrong(self):
         expected_badges = 0
 
-        mock_urllib3.return_value = Mock()
-        mock_urllib3_class_request = mock_urllib3.return_value.request.return_value
-        mock_urllib3_class_request.data = False
+        self.MockUrllib3_request.data = False
 
         get_information = GetInformation('Any url')
         badges = get_information.badges
