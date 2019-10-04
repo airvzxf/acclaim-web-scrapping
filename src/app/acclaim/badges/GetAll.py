a4fa5a7e9e339b3ace85abc90d51cce584ba0cba
Israel Roldan <israel.alberto.rv@gmail.com>
2019-10-04 01:48:01 -0500

    Addded the multi-thread implementation for request the badges.

--- src/app/acclaim/badges/GetAll.py
+++ src/app/acclaim/badges/GetAll.py
@@ -12,6 +12,10 @@ class GetAll:
     _REGULAR_EXPRESSION = r'c-grid-item c-grid-item--stack-lt-sm cr-public-organization-badge-template-grid-item.* ' + \
                           'href="/org/{company}/badge/([^"]+)"'
     _badges_id = []
+    _urls = []
+    _number_of_badges = 0
+    _multiprocess = True
+    _max_workers = None
     company = None
 
     def __init__(self, company: str) -> None:
@@ -32,21 +36,77 @@ class GetAll:
         :return: True if the process is completed and all the badges were retrieved.
         """
         from app.acclaim.badges.GetAllUrls import GetAllUrls
-        from urllib3 import PoolManager
-        from re import findall
         self._badges_id = []
         get_all_urls = GetAllUrls(self.company)
         if not get_all_urls.execute():
             return False
-        http = PoolManager()
-        for url in get_all_urls.get_urls:
-            response = http.request('GET', url)
-            try:
-                data = response.data.decode('utf-8')
-            except AttributeError:
-                continue
-            self._badges_id += findall(self._REGULAR_EXPRESSION, data)
+        self._number_of_badges = get_all_urls.get_badges
+        self._urls = get_all_urls.get_urls
+        if len(self._urls) < 1:
+            return False
+        self._multiprocessing_badges(self._multiprocess)
         return True
+
+    def _multiprocessing_badges(self, multiprocess: bool = True) -> None:
+        """
+        Get the badges using single or multi processing.
+        :type multiprocess: bool
+        :param multiprocess: If it's true then use multiprocessing other wise use a single processor.
+        :rtype: None
+        :return: None
+        """
+        from concurrent.futures.thread import ThreadPoolExecutor
+        from time import time
+        if multiprocess is False:
+            for url in self._urls:
+                self._badges_id += self._request_urls(url)
+        else:
+            init_time = time()
+            processors = self._max_workers
+            if processors:
+                number_of_urls = len(self._urls)
+                if number_of_urls <= 0:
+                    processors = None
+                elif number_of_urls < processors:
+                    processors = number_of_urls
+
+            executor = ThreadPoolExecutor(processors)
+            pages_with_badges = executor.map(self._request_urls, self._urls)
+
+            difference = int(time() - init_time)
+            print('# -------------------------------------------')
+            print('# Execution time: ', difference)
+            print('# -------------------------------------------')
+            for badges_id in pages_with_badges:
+                self._badges_id += badges_id
+        self._number_of_badges = len(self._badges_id)
+
+    def _request_urls(self, url: str) -> list:
+        """
+        Request an URL and return the badges id.
+        :type url: str
+        :param url: Web page URL with the badges.
+        :rtype: list
+        :return: Return a list with the badges IDs.
+        """
+        from urllib3 import PoolManager
+        from re import findall
+        http = PoolManager()
+        response = http.request('GET', url)
+        try:
+            data = response.data.decode('utf-8')
+        except AttributeError:
+            return []
+        return findall(self._REGULAR_EXPRESSION, data)
+
+    @property
+    def get_number_of_badges(self) -> int:
+        """
+        Return the number of badges.
+        :rtype: int
+        :return: Number of badges
+        """
+        return self._number_of_badges
 
     @property
     def get_badges_id(self) -> list:
--- src/test/acclaim/badges/GetAllTestCase.py
+++ src/test/acclaim/badges/GetAllTestCase.py
@@ -20,12 +20,16 @@ class GetAllUrlsTestCase(unittest.TestCase):
 
         self.patch_get_all_urls = patch('app.acclaim.badges.GetAllUrls.GetAllUrls', autospec=True)
         self.MockGetAllUrls = self.patch_get_all_urls.start()
-        self.MockGetAllUrls_execute = self.MockGetAllUrls.return_value.execute
 
         self.patch_urllib3 = patch('urllib3.PoolManager', autospec=True)
         self.MockUrllib3 = self.patch_urllib3.start()
         self.MockUrllib3_request = self.MockUrllib3.return_value.request
         self.MockUrllib3_requested = self.MockUrllib3_request.return_value
+        self.MockGetAllUrls_execute = self.MockGetAllUrls.return_value.execute
+
+        self.patch_thread_pool_executor = patch('concurrent.futures.thread.ThreadPoolExecutor', autospec=True)
+        self.MockThreadPoolExecutor = self.patch_thread_pool_executor.start()
+        self.MockThreadPoolExecutor_map = self.MockThreadPoolExecutor.return_value.map
 
     def tearDown(self) -> None:
         """
@@ -35,8 +39,9 @@ class GetAllUrlsTestCase(unittest.TestCase):
         """
         self.patch_get_all_urls.stop()
         self.patch_urllib3.stop()
+        self.patch_thread_pool_executor.stop()
 
-    def test_instance_the_company_attribute(self):
+    def test_the_company_attribute_in_the_instance(self):
         get_all = GetAll(self.company)
         company = get_all.company
         self.assertEqual(self.company, company)
@@ -49,11 +54,31 @@ class GetAllUrlsTestCase(unittest.TestCase):
         url = get_all._REGULAR_EXPRESSION
         self.assertEqual(expected_url, url)
 
+    def test_get_number_of_badges_from_the_class_get_all_urls(self):
+        expected_badges = 29843
+
+        self.MockGetAllUrls.return_value.get_badges = expected_badges
+
+        get_all = GetAll(self.company)
+        get_all._multiprocessing_badges = lambda x: x
+        get_all.execute()
+        number_of_badges = get_all.get_number_of_badges
+        self.assertEqual(expected_badges, number_of_badges)
+
     def test_call_the_class_get_all_urls(self):
         get_all = GetAll(self.company)
         get_all.execute()
         self.MockGetAllUrls.assert_called_once_with(self.company)
         self.MockGetAllUrls_execute.assert_called_once()
+
+    def test_when_get_zero_urls(self):
+        expected_executed = False
+
+        self.MockGetAllUrls.return_value.get_urls = []
+
+        get_all = GetAll(self.company)
+        executed = get_all.execute()
+        self.assertEqual(expected_executed, executed)
 
     def test_when_an_invalid_call_the_class_get_all_urls_returns_false(self):
         expected_executed = False
@@ -91,25 +116,7 @@ class GetAllUrlsTestCase(unittest.TestCase):
         self.MockUrllib3_requested.data = b''
 
         get_all = GetAll(self.company)
-        get_all.execute()
-        self.MockUrllib3_request.assert_has_calls(expected_calls)
-
-    def test_when_the_data_request_is_invalid(self):
-        urls = [
-            'Url #1',
-            'Url #2',
-            'Url #3',
-        ]
-        expected_calls = [
-            call('GET', urls[0]),
-            call('GET', urls[1]),
-            call('GET', urls[2]),
-        ]
-
-        self.MockGetAllUrls.return_value.get_urls = urls
-        self.MockUrllib3_requested.data = None
-
-        get_all = GetAll(self.company)
+        get_all._multiprocess = False
         get_all.execute()
         self.MockUrllib3_request.assert_has_calls(expected_calls)
 
@@ -137,14 +144,18 @@ class GetAllUrlsTestCase(unittest.TestCase):
             badges_ids[1],
             badges_ids[2],
         ]
+        expected_badges = len(badges_ids)
 
         self.MockGetAllUrls.return_value.get_urls = urls
         self.MockUrllib3_requested.data.decode.side_effect = responses
 
         get_all = GetAll(self.company)
+        get_all._multiprocess = False
         get_all.execute()
         badges_ids = get_all.get_badges_id
+        number_of_badges = get_all.get_number_of_badges
         self.assertEqual(expected_badges_ids, badges_ids)
+        self.assertEqual(expected_badges, number_of_badges)
 
     def test_get_two_valid_badges_id(self):
         base_url = r'<div class="c-grid-item c-grid-item--stack-lt-sm cr-public-organization-badge-' + \
@@ -174,6 +185,101 @@ class GetAllUrlsTestCase(unittest.TestCase):
         self.MockUrllib3_requested.data.decode.side_effect = responses
 
         get_all = GetAll(self.company)
+        get_all._multiprocess = False
         get_all.execute()
         badges_ids = get_all.get_badges_id
         self.assertEqual(expected_badges_ids, badges_ids)
+
+    def test_when_the_data_request_is_invalid(self):
+        urls = [
+            'Url #1',
+            'Url #2',
+            'Url #3',
+        ]
+        expected_calls = [
+            call('GET', urls[0]),
+            call('GET', urls[1]),
+            call('GET', urls[2]),
+        ]
+
+        self.MockGetAllUrls.return_value.get_urls = urls
+        self.MockUrllib3_requested.data = None
+
+        get_all = GetAll(self.company)
+        get_all._multiprocess = False
+        get_all.execute()
+        self.MockUrllib3_request.assert_has_calls(expected_calls)
+
+    def test_multiprocessor(self):
+        urls = [
+            'Url #1',
+            'Url #2',
+            'Url #3',
+        ]
+
+        get_all = GetAll(self.company)
+        get_all._urls = urls
+        get_all._multiprocessing_badges()
+        self.MockThreadPoolExecutor_map.assert_called_once_with(get_all._request_urls, get_all._urls)
+
+    def test_multiprocessor_when_max_workers_is_None_and_number_of_badges_exists(self):
+        expected_max_processor = None
+
+        get_all = GetAll(self.company)
+        get_all._number_of_badges = 1987
+        get_all._multiprocessing_badges()
+        self.MockThreadPoolExecutor.assert_called_once_with(expected_max_processor)
+
+    def test_multiprocessor_when_max_workers_is_greater_than_badges_but_badges_is_zero(self):
+        expected_max_processor = 999
+        badges = 0
+        expected_processor = None
+
+        get_all = GetAll(self.company)
+        get_all._max_workers = expected_max_processor
+        get_all._number_of_badges = badges
+        get_all._multiprocessing_badges()
+        self.MockThreadPoolExecutor.assert_called_once_with(expected_processor)
+
+    def test_multiprocessor_when_max_workers_is_greater_than_badges(self):
+        max_workers = 999
+        urls = ['', '', '', '', '', ]
+        expected_workers = len(urls)
+
+        get_all = GetAll(self.company)
+        get_all._max_workers = max_workers
+        get_all._urls = urls
+        get_all._multiprocessing_badges()
+        self.MockThreadPoolExecutor.assert_called_once_with(expected_workers)
+
+    def test_multiprocessor_when_max_workers_is_less_than_badges(self):
+        urls = ['', '', '', '', '', ]
+        expected_workers = 2
+
+        get_all = GetAll(self.company)
+        get_all._max_workers = expected_workers
+        get_all._urls = urls
+        get_all._multiprocessing_badges()
+        self.MockThreadPoolExecutor.assert_called_once_with(expected_workers)
+
+    def test_multiprocessor_get_badges_ids(self):
+        badges_1 = [
+            'hello-world',
+            'Nikki-93-Micah',
+            'what.eVer.123',
+        ]
+        badges_2 = [
+            'cool',
+            'I-am-badge'
+        ]
+        pages_with_badges = [
+            badges_1,
+            badges_2
+        ]
+        expected_badges_id = badges_1 + badges_2
+
+        self.MockThreadPoolExecutor_map.return_value = pages_with_badges
+
+        get_all = GetAll(self.company)
+        get_all._multiprocessing_badges()
+        self.assertEqual(expected_badges_id, get_all._badges_id)
